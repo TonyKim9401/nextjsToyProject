b496ba45ed893a2680796bc467496d0cf6b13795
ktony <k.tony9412@gmail.com>
2025-02-23 23:53:07 -0500

    feat How to use Props
    To send variables from parent component to child component use Props
    
    1. add {key = value} on child component tag from parent component
    2. child component use 'props' and access to props.key to take out the value from parent component
    
    use props to make components for improving re-usablility
    
    ex) Banner, ColorButton

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,19 +1,25 @@
-import Hello from "./data.js";
-
 export default function Cart() {
+  let cartList = ["Tomatoes", "Pasta"];
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <Hello />
-      <CartItem />
+      {cartList.map((cartProduct, i) => {
+        return <CartItem item={cartProduct} />;
+      })}
+      <Banner cardName="Master" />
+      <Banner cardName="AMAX" />
     </div>
   );
 }
 
-function CartItem() {
+function Banner(props) {
+  return <h5>{props.cardName} card is on no interest event now!</h5>;
+}
+
+function CartItem(props) {
   return (
     <div className="cart-item">
-      <p>product name</p>
+      <p>product name: {props.item}</p>
       <p>$40</p>
       <p>1</p>
     </div>
