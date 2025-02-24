4b99245a4cf7c00576a9af2f176ed37929cfa8f3
ktony <k.tony9412@gmail.com>
2025-02-24 00:37:48 -0500

    feat What is useState?
    when variables are changed on the web application, useState syntax helps to reflect them immediately with auto rendering
    
    import {useState} from "react"
    
    let [variable, variableFunction] = useState({initValue})

--- app/list/page.js
+++ app/list/page.js
@@ -1,7 +1,11 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function List() {
   let products = ["Tomatoes", "Pasta", "Coconut"];
+  // useState -> auto rendering reflecting its change!
+  let [stock, useStock] = useState(0);
 
   return (
     <div>
@@ -11,6 +15,24 @@ export default function List() {
           <div className="food" key={i}>
             <img src={`food${i}.png`} className="food-img" />
             <h4>{product} $40</h4>
+            <button
+              onClick={() => {
+                if (stock === 0) return;
+                // change helper(change value)
+                useStock(stock - 1);
+              }}
+            >
+              -
+            </button>
+            <span> {stock} </span>
+            <button
+              onClick={() => {
+                // change helper(change value)
+                useStock(stock + 1);
+              }}
+            >
+              +
+            </button>
           </div>
         );
       })}
